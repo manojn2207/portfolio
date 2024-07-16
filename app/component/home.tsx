19e03e4aa58d56f7a2efdc45923fa83ed4a5ee8a
ManojN22 <manojkumar22072001@gmail.com>
2024-07-16 09:11:04 -0400

    time line

---
+++ app/component/home.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link"
+import {  ScriptProps } from "next/script"
+
+export const AboutMe = ()=>{
+    return <div>
+         <div className="container flex items-center">
+          <span className="text-4xl font-semibold mr-10">
+            Manoj Nandakumar.
+          </span>
+          <Link href="/About-me">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </Link>
+        </div>
+        <div className="">
+          I'm Manoj Nandakumar, an MS CS graduate from SUNY at Stony Brook with
+          a year of software engineering experience. Passionate about creating
+          seamless user experiences, I've worked on projects involving Flutter
+          apps, SaaS products, and responsive web design. I enjoy solving
+          complex problems and continuously learning new technologies.
+          Currently, I'm focused on refining my skills in front-end development
+          and technical support.
+        </div>
+    </div>
+}
+import { Timeline } from "../../public/details";
+export const HorizontalTimeline = () => {
+  return (
+    <div className="timeline flex">
+      {Timeline.map((event, index) => (
+        <div key={index} className="timelineEvent flex-grow">
+          <div className="circle bg-primary-light"></div>
+          <div className="timelineContent">
+            <h3>{event.date}</h3>
+            <p>{event.description}</p>
+          </div>
+          <div className="line bg-primary-light"></div>
+        
+        </div>
+      ))}
+    </div>
+  );
+}
+ 
---
+++ public/details.ts
@@ -0,0 +1,17 @@
+interface timelineType {
+date:string,
+description:string
+}
+export const Timeline:Array<timelineType> = [
+    { date: '2024-01-01', description: 'New Year\'s Day' },
+    { date: '2024-02-01', description: 'Valentine\'s Day' },
+    { date: '2024-07-04', description: 'Independence Day' },
+    { date: '2024-01-01', description: 'New Year\'s Day' },
+    // { date: '2024-02-01', description: 'Valentine\'s Day' },
+    // { date: '2024-07-04', description: 'Independence Day' },
+    // { date: '2024-01-01', description: 'New Year\'s Day' },
+    // { date: '2024-02-01', description: 'Valentine\'s Day' },
+    // { date: '2024-07-04', description: 'Independence Day' },
+    // { date: '2024-02-01', description: 'Valentine\'s Day' },
+    // Add more events as needed
+  ];
